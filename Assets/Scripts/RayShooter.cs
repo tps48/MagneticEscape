a225ba173c5114847531b4bb8450d5c8df34af9b
tps48 <tps48@case.edu>
2019-04-08 16:33:22 -0400

    Modified player speed. Added energy bar for magnet.

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -6,62 +6,86 @@ using System.Collections;
 
 public class RayShooter : MonoBehaviour {
 	private Camera _camera;
+    private Light _light;
+    [SerializeField] GameObject spotLight;
+    public float barDisplay;
+    public Vector2 pos = new Vector2(20, 40);
+    public Vector2 size = new Vector2(600, 200);
+    private Texture2D emptyTex;
+    private Texture2D fullTex;
 
-	public Texture reticle;
+    public Texture reticle;
 
 	void Start() {
 		_camera = GetComponent<Camera>();
-
-		//Cursor.lockState = CursorLockMode.Locked;
-		//Cursor.visible = false;
+        _light = spotLight.GetComponent<Light>();
+		
 	}
 
-	void OnGUI() {
-		/*int size = 12;
-		float posX = _camera.pixelWidth/2 - size/4;
-		float posY = _camera.pixelHeight/2 - size/2;
-		//GUI.Label(new Rect(posX, posY, size, size), "*");
-		GUI.DrawTexture(new Rect(posX, posY, size, size), reticle);*/
-	}
+    void OnGUI()
+    {
 
-	void Update() {
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyTex);
+
+
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), fullTex);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+    void Update()
+    {
         Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+
         Ray ray = _camera.ScreenPointToRay(point);
-        RaycastHit hit1;
         Debug.DrawRay(point, ray.direction, Color.red, 20, true);
-		if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit)) {
-				GameObject hitObject = hit.transform.gameObject;
-				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
-				if (target != null) {
-					target.ReactToHit(this.transform.position, 1);
-					Messenger.Broadcast(GameEvent.ENEMY_HIT);
-				} 
-			}
-		}
-        else if (Input.GetMouseButton(1) && !EventSystem.current.IsPointerOverGameObject())
+        if (Input.GetMouseButton(0) && barDisplay > 0.01)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            barDisplay -= Time.deltaTime * 0.2f;
+            if (barDisplay > 0.011)
+            {
+                _light.color = new Color(250, 0, 5);
+            }
+
+            RaycastHit[] hits = Physics.RaycastAll(ray, 100f);
+            foreach (RaycastHit hit in hits)
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if (target != null)
+                {
+                    target.ReactToHit(this.transform.position, 1);
+                }
+            }
+        }
+        else if (Input.GetMouseButton(1) && barDisplay > 0.01)
+        {
+            barDisplay -= Time.deltaTime * 0.2f;
+            if (barDisplay > 0.011)
+            {
+                _light.color = new Color(0, 212, 250);
+            }
+            RaycastHit[] hits = Physics.RaycastAll(ray, 100f);
+            foreach (RaycastHit hit in hits)
             {
                 GameObject hitObject = hit.transform.gameObject;
                 ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
                 if (target != null)
                 {
                     target.ReactToHit(this.transform.position, -1);
-                    Messenger.Broadcast(GameEvent.ENEMY_HIT);
                 }
+            }
+        }
+        else
+        {
+            _light.color = new Color(215, 166, 34);
+            if (barDisplay < 1)
+            {
+                barDisplay += Time.deltaTime * 0.15f;
             }
         }
     }
 
-	private IEnumerator SphereIndicator(Vector3 pos) {
-		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-		sphere.transform.position = pos;
-
-		yield return new WaitForSeconds(1);
-
-		Destroy(sphere);
-	}
 }
---
+++ Assets/UI_Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Controller : MonoBehaviour
+{
+    public float barDisplay;
+    public Vector2 pos = new Vector2(20, 40);
+    public Vector2 size = new Vector2(600, 200);
+    private Texture2D emptyTex;
+    private Texture2D fullTex;
+
+    void OnGUI()
+    {
+        //emptyTex = Texture2D.blackTexture;
+        //fullTex = Texture2D.whiteTexture;
+        //emptyTex.Resize(1, 1);
+        //fullTex.Resize(1, 1);
+
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyTex);
+
+
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), fullTex);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+    void Update()
+    {
+
+        if (barDisplay < 1)
+        {
+            barDisplay += Time.deltaTime * 0.05f;
+        }
+
+    }
+}

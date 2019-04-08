30201603caddfc5c4f6e87d41f1a664d63622819
tps48 <tps48@case.edu>
2019-04-08 14:02:58 -0400

    Player rotation mapped to mouse. Minor fixes

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -14,46 +14,26 @@ using System.Collections;
 //   -> Set the mouse look to use MouseY. (You want the camera to tilt up and down like a head. The character already turns.)
 
 [AddComponentMenu("Control Script/Mouse Look")]
+
+
 public class MouseLook : MonoBehaviour {
-	public enum RotationAxes {
-		MouseXAndY = 0,
-		MouseX = 1,
-		MouseY = 2
-	}
-	public RotationAxes axes = RotationAxes.MouseXAndY;
 
-	public float sensitivityHor = 9.0f;
-	public float sensitivityVert = 9.0f;
-	
-	public float minimumVert = -45.0f;
-	public float maximumVert = 45.0f;
-
-	private float _rotationX = 0;
-	
-	void Start() {
+    
+    void Start() {
 		// Make the rigid body not change rotation
 		Rigidbody body = GetComponent<Rigidbody>();
 		if (body != null)
 			body.freezeRotation = true;
-	}
+
+    }
 
 	void Update() {
-		if (axes == RotationAxes.MouseX) {
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
-		}
-		else if (axes == RotationAxes.MouseY) {
-			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-			_rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
-			
-			transform.localEulerAngles = new Vector3(_rotationX, transform.localEulerAngles.y, 0);
-		}
-		else {
-			float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityHor;
 
-			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
-			_rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.LookAt(new Vector3(mousePos.x, 0, mousePos.z));
 
-			transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-		}
-	}
+
+    }
+    
+
 }

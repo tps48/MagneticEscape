252d4a0fa71b53813d2e50217d3e73e2584a8084
David Pietrow <dgp38@case.edu>
2019-04-08 22:25:58 -0400

    Debugging enemy collision

---
+++ Assets/Scripts/EnemyReactiveTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReactiveTarget : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collider collision)
+    {
+        Debug.Log("collision name = " + collision.gameObject.name);
+        if (collision.gameObject.name == "enemy" )
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}

961e9c799913c66f11e2211e35e4a89642579ffc
Kevin Szmyd <kps59@case.edu>
2019-04-08 22:36:19 -0400

    added level
    simple button tutorial

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+	[SerializeField] private GameObject plate;
+	[SerializeField] private GameObject door;
+	[SerializeField] private Material materialOn;
+	[SerializeField] private Material materialOff;
+	
+	void OnCollisionEnter (Collision col) {
+		 plate.GetComponent<MeshRenderer> ().material = materialOn;
+		 door.SetActive(false);
+    }
+	
+	void OnCollisionExit(Collision collisionInfo) {
+         plate.GetComponent<MeshRenderer> ().material = materialOff;
+		 door.SetActive(true);
+    }
+}

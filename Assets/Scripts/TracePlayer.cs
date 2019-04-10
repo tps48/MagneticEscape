9b6a265589f22acd33891a847b2d5db4fa157024
David Pietrow <dgp38@case.edu>
2019-04-09 23:18:01 -0400

    Added enemy fireballs and rotation upon seeing the player

---
+++ Assets/Scripts/TracePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float range;
+    Vector3 distance;
+    Vector3 playerposition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        range = 500.0f;
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Player Position = " + player.transform.position);
+        Debug.Log("Enemy Position = " + this.transform.position);
+        if ((player.transform.position - this.transform.position).sqrMagnitude < range)
+        {
+            transform.LookAt(player.transform);
+        }
+
+
+    }
+}
--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -18,21 +18,29 @@ public class WanderingAI : MonoBehaviour
 
     void Update()
     {
-        if (_alive == true)
+        if (_alive)
         {
 
-
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+            }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        
 
+        public void SetAlive(bool alive)
+        {
+            _alive = alive;
+        }
     }
-
-    public void SetAlive(bool alive)
-    {
-        _alive = alive;
-    }
-}

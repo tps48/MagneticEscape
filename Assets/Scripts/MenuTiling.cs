07fecac2a63ceefbb3e85f32ef19827200b073b5
Kevin Szmyd <kps59@case.edu>
2019-04-21 22:46:14 -0400

    made menu
    2d menu drops to lvl1

---
+++ Assets/Scripts/MenuTiling.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MenuTiling : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    float scrollSpeed = 0.5f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        // Animates main texture scale in a funky way!
+        float scaleX = Time.time * 0.5f + 1;
+        float scaleY = Mathf.Sin(Time.time) * 0.5f + 1;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(scaleX, scaleY));
+    }
+}
---
+++ Assets/Scripts/UISceneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class UISceneController : MonoBehaviour {
+
+    [SerializeField] private GameObject quitButton;
+    [SerializeField] private GameObject replayButton;
+    [SerializeField] private GameObject winAward;
+
+	// Use this for initialization
+	void Start() {
+    }
+	
+
+	public void Restart() {
+		Application.LoadLevel("Lvl1");
+	}
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void ChangeSize(string newSizeString) {
+        string[] newSize = newSizeString.Split(',');
+        PlayerPrefs.SetInt("ySize", int.Parse(newSize[0]));
+        PlayerPrefs.SetInt("xSize", int.Parse(newSize[1]));
+        Restart();
+    }
+}
